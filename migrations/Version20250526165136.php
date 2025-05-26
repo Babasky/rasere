f96b501cc51d8793f59ccd046920b91825a2fe25
Babasky <boureimamaiga67@gmail.com>
2025-05-26 16:58:40 +0000

    change photo column type in forum table

--- migrations/Version20250526165136.php
+++ migrations/Version20250526165136.php
@@ -54,6 +54,9 @@ final class Version20250526165136 extends AbstractMigration
             CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT '(DC2Type:json)', password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
+            INSERT INTO `user` (email, roles, password, firstname, lastname) VALUES ('admin@rasere.com', '["ROLE_USER"]', '$2y$13$qW/qEVw7JQ188FfkcpvpVepilq6ExR5ivt/gYfMPyvLUTtYW1F0XK', 'Admin', 'Rasere')
+        SQL);
+        $this->addSql(<<<'SQL'
             CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
